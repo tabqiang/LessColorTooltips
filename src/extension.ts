afbea620e7609140fb2e30aa6368311c02df229c
Caps <838269741@qq.com>
2024-08-27 19:10:09 +0800

    fix: rgb fix,add log category

--- src/extension.ts
+++ src/extension.ts
@@ -2,130 +2,133 @@ import * as vscode from "vscode"
 import * as fs from "fs"
 import * as path from "path"
 
+const outputChannel = vscode.window.createOutputChannel("Less Color Tooltip")
 export function activate(context: vscode.ExtensionContext) {
-    const provider = vscode.languages.registerHoverProvider(["less", "vue"], {
-        async provideHover(document, position, token) {
-            const fileName = document.fileName
-            if (fileName.endsWith(".vue")) {
-                const text = document.getText()
-                const styleTagRegex = /<style\s+lang=["']less["'][^>]*>([\s\S]*?)<\/style>/g
-                let match
-                while ((match = styleTagRegex.exec(text)) !== null) {
-                    const styleContent = match[1]
-                    const startPos = document.positionAt(match.index)
-                    const endPos = document.positionAt(match.index + match[0].length)
-                    const range = new vscode.Range(startPos, endPos)
-                    if (range.contains(position)) {
-                        const wordRange = document.getWordRangeAtPosition(
-                            position,
-                            /#[0-9a-fA-F]{6}|#[0-9a-fA-F]{3}|rgba?\(\s*\d+\s*,\s*\d+\s*,\s*\d+(?:\s*,\s*\d+)?\s*\)/i
-                        )
-                        if (wordRange) {
-                            const word = document.getText(wordRange)
-                            const properties = await findPropertiesInProject(word)
-                            if (properties.length > 0) {
-                                const formattedProperties = properties
-                                    .map(prop => `@${prop}`)
-                                    .join("\n\n")
-                                return new vscode.Hover(
-                                    new vscode.MarkdownString(formattedProperties)
-                                )
-                            }
-                        }
-                    }
-                }
-            } else {
-                const range = document.getWordRangeAtPosition(
-                    position,
-                    /#[0-9a-fA-F]{6}|#[0-9a-fA-F]{3}|rgba?\(\s*\d+\s*,\s*\d+\s*,\s*\d+(?:\s*,\s*\d+)?\s*\)/i
-                )
-                if (range) {
-                    const word = document.getText(range)
-                    const properties = await findPropertiesInProject(word)
-                    if (properties.length > 0) {
-                        const formattedProperties = properties
-                            .map(prop => `@${prop}`)
-                            .join("\n\n")
-                        return new vscode.Hover(
-                            new vscode.MarkdownString(formattedProperties)
-                        )
-                    }
-                }
-            }
-            return null
-        },
-    })
+	outputChannel.appendLine(
+		'Congratulations, your extension "less-color-tooltip" is now active!'
+	)
+	const provider = vscode.languages.registerHoverProvider(["less", "vue"], {
+		async provideHover(document, position, token) {
+			const fileName = document.fileName
+			const colorRegex =
+				/#[0-9a-fA-F]{6}|#[0-9a-fA-F]{3}|rgba?\(\s*\d+\s*,\s*\d+\s*,\s*\d+(?:\s*,\s*\d+(\.\d+)?)?\s*\)/i
 
-    context.subscriptions.push(provider)
+			if (fileName.endsWith(".vue")) {
+				const text = document.getText()
+				const styleTagRegex =
+					/<style\s+lang=["']less["'][^>]*>([\s\S]*?)<\/style>/g
+				let match
+				while ((match = styleTagRegex.exec(text)) !== null) {
+					const styleContent = match[1]
+					const startPos = document.positionAt(match.index)
+					const endPos = document.positionAt(match.index + match[0].length)
+					const range = new vscode.Range(startPos, endPos)
+					if (range.contains(position)) {
+						const wordRange = document.getWordRangeAtPosition(
+							position,
+							colorRegex
+						)
+						if (wordRange) {
+							let word = document.getText(wordRange)
+							const properties = await findPropertiesInProject(word)
+							if (properties.length > 0) {
+								const formattedProperties = properties
+									.map(prop => `@${prop}`)
+									.join("\n\n")
+								return new vscode.Hover(
+									new vscode.MarkdownString(formattedProperties)
+								)
+							}
+						}
+					}
+				}
+			} else {
+				const range = document.getWordRangeAtPosition(position, colorRegex)
+				if (range) {
+					let word = document.getText(range)
+					const properties = await findPropertiesInProject(word)
+					if (properties.length > 0) {
+						const formattedProperties = properties
+							.map(prop => `@${prop}`)
+							.join("\n\n")
+						return new vscode.Hover(
+							new vscode.MarkdownString(formattedProperties)
+						)
+					}
+				}
+			}
+			return null
+		},
+	})
+
+	context.subscriptions.push(provider)
 }
 
 async function findPropertiesInProject(color: string): Promise<string[]> {
-    const properties: string[] = []
-    const config = vscode.workspace.getConfiguration("lessColorTooltip")
-    const relativeFilePath = config.get<string>("filePath")
-    if (!relativeFilePath) {
-        return properties
-    }
+	const properties: string[] = []
+	const config = vscode.workspace.getConfiguration("lessColorTooltip")
+	const relativeFilePath = config.get<string>("filePath")
+	if (!relativeFilePath) {
+		return properties
+	}
 
-    const rootPath = vscode.workspace.workspaceFolders?.[0].uri.fsPath
-    if (!rootPath) {
-        return properties
-    }
+	const rootPath = vscode.workspace.workspaceFolders?.[0].uri.fsPath
+	if (!rootPath) {
+		return properties
+	}
 
-    const filePath = path.join(rootPath, relativeFilePath)
-    const stat = await fs.promises.stat(filePath)
-    if (stat.isDirectory()) {
-        await searchFiles(filePath, color, properties)
-    } else {
-        await searchFile(filePath, color, properties)
-    }
+	const filePath = path.join(rootPath, relativeFilePath)
+	const stat = await fs.promises.stat(filePath)
+	if (stat.isDirectory()) {
+		await searchFiles(filePath, color, properties)
+	} else {
+		await searchFile(filePath, color, properties)
+	}
 
-    return properties
+	return properties
 }
 
 async function searchFiles(dir: string, color: string, properties: string[]) {
-    const files = await fs.promises.readdir(dir)
-    for (const file of files) {
-        const filePath = path.join(dir, file)
-        const stat = await fs.promises.stat(filePath)
-        if (stat.isDirectory()) {
-            await searchFiles(filePath, color, properties)
-        } else if (filePath.endsWith(".less") || filePath.endsWith(".css")) {
-            await searchFile(filePath, color, properties)
-        }
-    }
+	const files = await fs.promises.readdir(dir)
+	for (const file of files) {
+		const filePath = path.join(dir, file)
+		const stat = await fs.promises.stat(filePath)
+		if (stat.isDirectory()) {
+			await searchFiles(filePath, color, properties)
+		} else if (filePath.endsWith(".less") || filePath.endsWith(".css")) {
+			await searchFile(filePath, color, properties)
+		}
+	}
 }
 
 async function searchFile(
-    filePath: string,
-    color: string,
-    properties: string[]
+	filePath: string,
+	color: string,
+	properties: string[]
 ) {
-    const content = await fs.promises.readFile(filePath, "utf-8")
-    const lines = content.split("\n")
-    const regex = new RegExp(
-        `--(\\w+(?:-\\w+)*):\\s*${escapeRegExp(color)}\\b`,
-        "gi"
-    )
+	const content = await fs.promises.readFile(filePath, "utf-8")
+	const lines = content.split("\n")
 
-    for (let i = 0; i < lines.length; i++) {
-        let match
-        while ((match = regex.exec(lines[i])) !== null) {
-            let property = match[1]
-            if (i > 0 && lines[i - 1].trim().startsWith("/*")) {
-                const comment = lines[i - 1]
-                    .trim()
-                    .replace(/\/\*|\*\//g, "")
-                    .trim()
-                property = `${property}: ${comment}`
-            }
-            properties.push(property)
-        }
-    }
+	for (let i = 0; i < lines.length; i++) {
+
+		if (lines[i].includes(color)) {
+			//兼容rgb格式
+			//从lines[i]数据中--popover-shadow-color: rgba(205, 210, 218,0.4)先配出--popover-shadow-color，再去掉前面的--和空格
+			let property = lines[i].split(":")[0].replace(/--| /g, "")
+			if (i > 0 && lines[i - 1].trim().startsWith("/*")) {
+				const comment = lines[i - 1]
+					.trim()
+					.replace(/\/\*|\*\//g, "")
+					.trim()
+				property = `${property}: ${comment}`
+			}
+			properties.push(property)
+		}
+	}
 }
 
 function escapeRegExp(string: string): string {
-    return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&")
+	return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&")
 }
 
 export function deactivate() {}
